d97c64cf22f7509b2be7c4403ecc10f48127f2ea
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2024-11-16 08:08:15 +0530

    term and condition page done

--- src/app/term/page.tsx
+++ src/app/term/page.tsx
@@ -3,108 +3,114 @@ import PublicLayout from "../layouts";
 export default function Page() {
   return (
     <PublicLayout>
-      <div className="main-container py-5">
-        Who We Are Our website address is: http://demo.iplusresearch.in. TERMS
-        By accessing this web site, you are agreeing to be bound by these
-        website Terms and Conditions of Use, all applicable laws and
-        regulations, and agree that you are responsible for compliance with any
-        applicable local laws. If you do not agree with any of these terms, you
-        are prohibited from using or accessing this site. The materials
-        contained in this web site are protected by applicable copyright and
-        trade mark law. By registering on www.iplusresearch.in you explicitly
-        agree to be contacted by our personnel/send you SMS’s related to our
-        services even if the contact number you have entered is on DND (Do not
-        Disturb). USE LICENSE a. Permission is granted to temporarily download
-        one copy of the materials (information) on iplusresearch’s web site for
-        personal, non-commercial transitory viewing only. This is the grant of a
-        license, not a transfer of title, and under this license you may not: i.
-        modify or copy the materials; ii. use the materials for any commercial
-        purpose, or for any public display (commercial or non-commercial); iii.
-        attempt to decompile or reverse engineer any software contained on
-        iplusresearch’s web site; iv. remove any copyright or other proprietary
-        notations from the materials; or v. transfer the materials to another
-        person or “mirror” the materials on any other server. b. This license
-        shall automatically terminate if you violate any of these restrictions
-        and may be terminated by iplusresearch – Research Analyst at any time.
-        Upon terminating your viewing of these materials or upon the termination
-        of this license, you must destroy any downloaded materials in your
-        possession whether in electronic or printed format. DISCLAIMER The
-        materials on iplusresearch’s web site are provided “as is”.
-        iplusresearch – Research Analyst makes no warranties, expressed or
-        implied, and hereby disclaims and negates all other warranties,
-        including without limitation, implied warranties or conditions of
-        merchantability, fitness for a particular purpose, or non-infringement
-        of intellectual property or other violation of rights. Further,
-        iplusresearch – Research Analyst does not warrant or make any
-        representations concerning the accuracy, likely results, or reliability
-        of the use of the materials on its Internet web site or otherwise
-        relating to such materials or on any sites linked to this site.
-        LIMITATIONS In no event shall iplusresearch – Research Analyst or its
-        suppliers be liable for any damages (including, without limitation,
-        damages for loss or profit, monetary and/or non monetary,) arising out
-        of the use or inability to use the materials communicated by
-        iplusresearch – Research Analyst and its Employees. REVISIONS AND ERRATA
-        The materials appearing on iplusresearch’s web site could include
-        technical, typographical, or photographic errors. iplusresearch –
-        Research Analyst does not warrant that any of the materials on its web
-        site are accurate, complete, or current. iplusresearch – Research
-        Analyst may make changes to the materials contained on its web site at
-        any time without notice. iplusresearch – Research Analyst does not,
-        however, make any commitment to update the materials. LINKS
-        iplusresearch – Research Analyst has not reviewed all of the sites
-        linked to its Internet web site and is not responsible for the contents
-        of any such linked site. The inclusion of any link does not imply
-        endorsement by iplusresearch – Research Analyst of the site. Use of any
-        such linked web site is at the user’s own risk. SITE TERMS OF USE
-        MODIFICATIONS iplusresearch – Research Analyst may revise these terms of
-        use for its web site at any time without notice. By using this web site
-        you are agreeing to be bound by the then current version of these Terms
-        of Use. GOVERNING LAW Any claim relating to iplusresearch’s web site
-        shall be governed by the Indian laws, without regard to its conflict of
-        law provisions. General Terms and Conditions applicable to Use of a
-        Website. PRIVACY POLICY GENERAL PRIVACY Like most Websites iPlus
-        Research Analyst collects non-personally-identifying information of the
-        sort that web browsers and servers typically make available, such as the
-        browser type, language preference, referring site, and the date and time
-        of each visitor request. iplusresearch.in also collects potentially
-        personally-identifying information like Internet Protocol (IP) addresses
-        for security reasons. iPlus Research Analyst does not disclose
-        personally-identifying information other than as described below. And
-        visitors can always refuse to supply personally-identifying information,
-        with the caveat that it may prevent them from engaging in certain
-        website-related activities. PROTECTION OF CERTAIN PERSONALLY-IDENTIFYING
-        INFORMATION iPlus Research Analyst discloses potentially
-        personally-identifying and personally-identifying information only to
-        those of its employees, contractors and affiliated organizations that
-        (i) need to know that information in order to process it on iPlus
-        Research Analyst’ behalf or to provide services available at IPlus
-        Research Analyst’ websites, and (ii) that have agreed not to disclose it
-        to others. iPlus Research Analyst will not rent or sell potentially
-        personally-identifying and personally-identifying information to anyone.
-        If you are a registered user of iPlus Research Analyst website and have
-        supplied your email address, iPlus Research Analyst may occasionally
-        send you an email to tell you about new features, solicit your feedback,
-        or just keep you up to date with what’s going on with iPlus Research
-        Analyst and our products. iPlus Research Analyst takes all measures
-        reasonably necessary to protect against the unauthorized access, use,
-        alteration or destruction of potentially personally-identifying and
-        personally-identifying information. COOKIES A cookie is a string of
-        information that a website stores on a visitor’s computer, and that the
-        visitor’s browser provides to the website each time the visitor returns.
-        iPlus Research Analyst uses cookies to help IPlus Research Analyst
-        identify and track visitors, their usage of iPlus Research Analyst
-        website, and their website access preferences. IPlus Research Analyst
-        visitors who do not wish to have cookies placed on their computers
-        should set their browsers to refuse cookies before using iPlus Research
-        Analyst’ websites, with the drawback that certain features of iPlus
-        Research Analyst’ websites may not function properly without the aid of
-        cookies. PRIVACY POLICY CHANGES Although most changes are likely to be
-        minor, iPlus Research Analyst may change its Privacy Policy from time to
-        time, and in iPlus Research Analyst’ – Research Analyst sole discretion.
-        iPlus Research Analyst encourages visitors to frequently check this page
-        for any changes to its Privacy Policy. Your continued use of this site
-        after any change in this Privacy Policy will constitute your acceptance
-        of such change.
+      <div className="main-container py-5 flex flex-col gap-3 bg-purple-900/5">
+        <h1>Who We Are</h1>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          Our website address is: https://stoxxon.vercel.app/.
+        </p>
+        <h1>TERMS</h1>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          By accessing this web site, you are agreeing to be bound by these
+          website Terms and Conditions of Use, all applicable laws and
+          regulations, and agree that you are responsible for compliance with
+          any applicable local laws. If you do not agree with any of these
+          terms, you are prohibited from using or accessing this site. The
+          materials contained in this web site are protected by applicable
+          copyright and trade mark law. By registering on
+          https://stoxxon.vercel.app/ you explicitly agree to be contacted by
+          our personnel/send you SMS’s related to our services even if the
+          contact number you have entered is on DND (Do not Disturb).
+        </p>
+        <h1>USE LICENSE</h1>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          a. Permission is granted to temporarily download one copy of the
+          materials (information) on {`Stoxxon's`} web site for personal,
+          non-commercial transitory viewing only.
+        </p>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          This is the grant of a license, not a transfer of title, and under
+          this license you may not:
+        </p>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          i. modify or copy the materials;
+        </p>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          ii. use the materials for any commercial purpose, or for any public
+          display (commercial or non-commercial);
+        </p>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          iii. attempt to decompile or reverse engineer any software contained
+          on {`Stoxxon's`} web site;
+        </p>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          iv. remove any copyright or other proprietary notations from the
+          materials; or
+        </p>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          v. transfer the materials to another person or “mirror” the materials
+          on any other server.
+        </p>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          b. This license shall automatically terminate if you violate any of
+          these restrictions and may be terminated by stoxxon – Research Analyst
+          at any time. Upon terminating your viewing of these materials or upon
+          the termination of this license, you must destroy any downloaded
+          materials in your possession whether in electronic or printed format.
+        </p>
+        <h1>DISCLAIMER</h1>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          The materials on {`Stoxxon's`} web site are provided “as is”. stoxxon
+          – Research Analyst makes no warranties, expressed or implied, and
+          hereby disclaims and negates all other warranties, including without
+          limitation, implied warranties or conditions of merchantability,
+          fitness for a particular purpose, or non-infringement of intellectual
+          property or other violation of rights. Further, stoxxon – Research
+          Analyst does not warrant or make any representations concerning the
+          accuracy, likely results, or reliability of the use of the materials
+          on its Internet web site or otherwise relating to such materials or on
+          any sites linked to this site.
+        </p>
+        <h1>LIMITATIONS</h1>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          In no event shall stoxxon – Research Analyst or its suppliers be
+          liable for any damages (including, without limitation, damages for
+          loss or profit, monetary and/or non monetary,) arising out of the use
+          or inability to use the materials communicated by stoxxon – Research
+          Analyst and its Employees.
+        </p>
+        <h1>REVISIONS AND ERRATA</h1>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          The materials appearing on {`Stoxxon's`} web site could include
+          technical, typographical, or photographic errors. stoxxon – Research
+          Analyst does not warrant that any of the materials on its web site are
+          accurate, complete, or current. stoxxon – Research Analyst may make
+          changes to the materials contained on its web site at any time without
+          notice. stoxxon – Research Analyst does not, however, make any
+          commitment to update the materials.
+        </p>
+        <h1>LINKS</h1>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          stoxxon – Research Analyst has not reviewed all of the sites linked to
+          its Internet web site and is not responsible for the contents of any
+          such linked site. The inclusion of any link does not imply endorsement
+          by stoxxon – Research Analyst of the site. Use of any such linked web
+          site is at the user’s own risk.{" "}
+        </p>
+        <h1>SITE TERMS OF USE MODIFICATIONS </h1>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          stoxxon – Research Analyst may revise these terms of use for its web
+          site at any time without notice. By using this web site you are
+          agreeing to be bound by the then current version of these Terms of
+          Use.
+        </p>
+        <h1>GOVERNING LAW</h1>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          Any claim relating to {`Stoxxon's`} web site shall be governed by the
+          Indian laws, without regard to its conflict of law provisions.
+        </p>
+        <p className="text-gray-500 text-sm tracking-wider font-thin">
+          General Terms and Conditions applicable to Use of a Website.
+        </p>
       </div>
     </PublicLayout>
   );
