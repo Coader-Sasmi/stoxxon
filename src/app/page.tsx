33100b809bd71d7f5993ea4967e67ade0188a842
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2025-02-14 10:57:34 +0530

    hide the data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
-import {
-  About,
-  ContactUs,
-  Hero,
-  PricingSection,
-  TopServices,
-} from "@/components";
-import PublicLayout from "./layouts";
-
 export default function Home() {
   return (
-    <PublicLayout>
+    <>
+      <section className=" bg-black h-screen">
+        <div className="main-container flex h-full justify-center items-center ">
+          <div className="flex flex-col gap-6 text-gray-500">
+            <img src="./error.png" alt="image" className="w-20 h-auto" />
+            <h1 className="text-3xl tracking-wide">{`This site can't be reached`}</h1>
+            <p className="text-lg">
+              The web page at https://www.stoxxon.com/ might be temporarily down
+              or it may have moved permanently to a new web address.
+            </p>
+            <h4 className="text-lg">ERR_FAILED</h4>
+          </div>
+        </div>
+      </section>
+      {/* <PublicLayout>
       <Hero />
       <About />
       <TopServices />
       <PricingSection />
       <ContactUs />
-    </PublicLayout>
+    </PublicLayout> */}
+    </>
   );
 }
